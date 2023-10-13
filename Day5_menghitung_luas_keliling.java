da7d9c4e07584c2cf3c51ffdf7323e7ecf4df41f
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-13 21:38:31 +0800

    Day5

---
+++ Day5_menghitung_luas_keliling.java
@@ -0,0 +1,16 @@
+
+public class hari5 {
+    public static void main(String[] args) {
+        int panjang=15;
+        int lebar=30;
+        
+        
+        int rumus = panjang*lebar;
+        int kel = 2*(panjang+lebar);
+        System.out.println("luas    :"+rumus+"cm");
+        System.out.println("Keliling:"+kel+"cm");
+        
+        
+        
+    }
+}
