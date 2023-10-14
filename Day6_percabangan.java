a039e6d8db3744f397e6c0df5953db4314330b03
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-14 20:36:51 +0800

    Day6

---
+++ Day6_percabangan.java
@@ -0,0 +1,12 @@
+package javaapplication10;
+
+public class percabangan {
+    public static void main(String[] args) {
+        int angka = 50; 
+        if (angka > 49) {
+            System.out.println("Itu Besar");
+        } else { 
+            System.out.println("Itu Kecil"); //
+        }
+    }
+}
