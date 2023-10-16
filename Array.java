8c7e48e579b03c56f06013c90c11d7086cfcb59b
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-16 21:28:30 +0800

    Day8

---
+++ Array.java
@@ -0,0 +1,10 @@
+package day8;
+
+public class array {
+    public static void main(String[] args) {
+        String[] Buah = {"Apel","Anggur","Delima"};
+        
+        System.out.println(mahasiswa[0]);
+    }
+    
+}
