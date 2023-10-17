a6f9d1b54c027e1a1ead0895e1066b4ba16e885f
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-17 23:42:31 +0800

    Day9

---
+++ Penggunaan_ArrayList.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> daftarBuah = new ArrayList<>();
+
+        daftarBuah.add("Apel");
+        daftarBuah.add("Pisang");
+        daftarBuah.add("Jeruk");
+
+        System.out.println("Daftar Buah: " + daftarBuah);
+    }
+}
