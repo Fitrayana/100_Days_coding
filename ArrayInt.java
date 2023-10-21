8a51c79d1eeef54e687094ed19d8032fc4bb5d32
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-21 21:50:32 +0800

    Day13

---
+++ ArrayInt.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int[] angka = new int[5]; // Membuat array integer dengan 5 elemen
+        angka[0] = 5;
+        angka[1] = 10;
+        angka[2] = 15;
+
+
+        int nilai = angka[1]; // Akan menghasilkan 10
+        System.out.println("Nilai kedua Dalam Array Adalah:"+nilai);
+    }
+}
