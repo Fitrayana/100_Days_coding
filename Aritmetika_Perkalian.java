66e9943c73c57eb5fa211288b3ded13bf51f4d8f
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-23 16:42:24 +0800

    Day12

---
+++ Aritmetika_Perkalian.java
@@ -0,0 +1,10 @@
+public class Perkalian {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 7;
+
+        int hasil = angka1 * angka2;// Mengalikan angka1 dan angka2
+        System.out.println("Hasil perkalian dari " + angka1 + " dan " + angka2 + " adalah " + hasil);
+    }
+}
+
