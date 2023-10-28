00e82082ac24c161b193c93ed7aed1a29e290790
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-28 08:11:05 +0800

    Day20

---
+++ Array_1_Dimensi.java
@@ -0,0 +1,15 @@
+package Fitrayana;
+
+public class Array_1_Dimensi {
+
+    public static void main(String[] args) {
+        // membuat array 1 dimensi
+        int[] fitArray = {1, 2, 3, 4, 5};
+
+        //mengakses elem array
+        System.out.println(fitArray[0]);
+        System.out.println(fitArray[2]);
+
+    }
+
+}
