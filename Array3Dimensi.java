7889189ee0f8758f4f5383bd89786996a9e1d3d4
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-29 08:16:17 +0800

    Day21

---
+++ Array3Dimensi.java
@@ -0,0 +1,24 @@
+public class fitrayana {
+    public static void main(String[] args) {
+        // Mendefinisikan array tiga dimensi
+        int[][][] fitraArray = new int[3][4][5];
+
+        // Mengisi array dengan data
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 5; k++) {
+                    fitraArray[i][j][k] = i * 100 + j * 10 + k;
+                }
+            }
+        }
+
+        // Mengakses dan mencetak isi array
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 5; k++) {
+                    System.out.println("fitraArray[" + i + "][" + j + "][" + k + "] = " + arrayTigaDimensi[i][j][k]);
+                }
+            }
+        }
+    }
+}
