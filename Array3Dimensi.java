a724cbd8575442f7c12c04f1f593472b508f2d0a
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-10-29 08:17:48 +0800

    Day21

--- Array3Dimensi.java
+++ Array3Dimensi.java
@@ -16,7 +16,7 @@ public class fitrayana {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 4; j++) {
                 for (int k = 0; k < 5; k++) {
-                    System.out.println("fitraArray[" + i + "][" + j + "][" + k + "] = " + arrayTigaDimensi[i][j][k]);
+                    System.out.println("fitraArray[" + i + "][" + j + "][" + k + "] = " + fitraArray[i][j][k]);
                 }
             }
         }
