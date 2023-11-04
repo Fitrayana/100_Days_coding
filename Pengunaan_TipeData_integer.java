98b6534574b4e624df4edfc6e108133262b0202c
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-04 08:56:55 +0800

    Day27

---
+++ Pengunaan_TipeData_integer.java
@@ -0,0 +1,13 @@
+
+//Dalam Bahasa pemrograman Java terdapat beberapa tipe data yaitu: tipe data integer, byte, float, char, bolean, short,double dan long.Berikut contoh penggunaan tipe data integer
+public class Integer {
+    public static void main(String[] args) {
+        int nilaiPertama = 20;
+        int nilaiKedua = 5;
+
+        int hasil = nilaiPertama + nilaiKedua;
+
+        System.out.println("Hasil: " + hasil);
+    }
+}
+//nah tipe data integer merupakan tipe data yang memuat bilangan bulat 
