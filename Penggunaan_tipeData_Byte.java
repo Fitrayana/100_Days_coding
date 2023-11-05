b5909bdf8547731b3d77bf24a409f43ba28646e4
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-05 19:27:14 +0800

    Day28

---
+++ Penggunaan_tipeData_Byte.java
@@ -0,0 +1,15 @@
+//Berikut contoh penggunaan tipe data Byte
+//Tipe data byte digunakan untuk menyimpan bilangan bulat yang  kecil
+
+public class Byte {
+    public static void main(String[] args) {
+        byte nilaiByte = 40; // Mendeklarasikan variabel byte dengan nilai 40
+        System.out.println("Nilai byte: " + nilaiByte);
+        
+        // contoh operasi aritmatika dengan tipe data byte
+        byte nilai1 = 10;
+        byte nilai2 = 20;
+        byte hasil = (byte) (nilai1 + nilai2);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
