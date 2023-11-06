195038b1b6188a0aa5e3456d8ca53d41b104ca8d
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-06 23:07:11 +0800

    Day29

---
+++ Penggunaan _TipeData_Boolean.java
@@ -0,0 +1,16 @@
+// Berikut contoh penggunaan tipe data boolean, yang dimana tipe data boolean hanya menyimpan 2 nilai yaitu false dan true
+public class Boolean {
+    public static void main(String[] args) {
+
+        int nilaiRespon = 50;
+        boolean Lulus;
+
+        if (nilaiUts >= 70) {
+            Lulus = true;
+            System.out.println(Lulus);
+        } else {
+            Lulus = false;
+        }
+        System.out.println(Lulus);
+    }
+}
