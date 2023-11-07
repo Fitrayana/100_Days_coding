98e1ed631c9ae57241417687b0aa781a50b923a6
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-07 23:06:58 +0800

    Day30

---
+++ Penggunaan_TipeData_String.java
@@ -0,0 +1,9 @@
+public class String {
+    public static void main(String[] args) {
+        String nama = "Fitrayana";
+        String alamat = " No.23 Orobatu";
+     
+      System.out.println("Nama: " + nama);
+      System.out.println("Alamat: " + alamat);
+    }
+}
