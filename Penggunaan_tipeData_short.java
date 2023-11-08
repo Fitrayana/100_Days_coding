ea8f5b577b7a1248d3844e63085ea4247088c904
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-09 02:03:19 +0800

    Day31

---
+++ Penggunaan_tipeData_short.java
@@ -0,0 +1,11 @@
+
+public class Short {
+    public static void main(String[] args) {
+        // Mendeklarasikan variabel short
+        short iniShort = 1000;
+
+        // Menampilkan nilai variabel short
+        System.out.println("Nilai iniShort: " + iniShort);
+
+        
+        
