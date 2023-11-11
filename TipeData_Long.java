93cb7675503e97f9e2702101b9c6e05caa4e3906
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-11 20:05:54 +0800

    Day34

--- TipeData_Long.java
+++ TipeData_Long.java
@@ -1,4 +1,3 @@
-
 // Tipe data long merupakan salah satu tipe data primitif dalam bahasa Java yang digunakan untuk menyimpan nilai bilangan bulat yang lebih besar daripada yang dapat disimpan oleh tipe data int. Tipe data longdapat digunakan untuk menyimpan nilai dalam rentang yang lebih besar, yaitu dari -9,223,372,036,854,775,808 sampai 9,223,372,036,854,
 public class Long {
     public static void main(String[] args) {
