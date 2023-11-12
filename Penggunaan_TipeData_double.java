fbf0140ed739a3741041222ba11240f531990e15
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-12 22:29:30 +0800

    Day35

---
+++ Penggunaan_TipeData_double.java
@@ -0,0 +1,11 @@
+//tipe data double adalah salah satu tipe data primitif yang digunakan untuk menyimpan nilai bilangan pecahan
+public class Double{
+    public static void main(String[] args) {
+        double angka1 = 15.1;
+        double angka2 = 10.3;
+
+
+        double hasil = angka1 + angka2;
+        System.out.println("Hasil Penjumlahan: " + hasil);
+    }
+}
