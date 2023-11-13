2853de73401d4c59858d9b67590be0dbcc2c7afa
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-13 22:34:05 +0800

    Day36

---
+++ TipeData_BigInteger.java
@@ -0,0 +1,13 @@
+
+import java.math.BigInteger;
+
+public class BigInteger {
+    public static void main(String[] args) {
+        BigInteger nilai1 = new BigInteger("123456789102345678901234567890");
+        BigInteger nilai2 = new BigInteger("987654321098765432109876543210");
+
+        System.out.println("nilai 1 : " + nilai1);
+        System.out.println("nilai 2 : " + nilai2);
+    }
+}
+//Biginteger merupakan tipe data non Primitif dalam bahasa pemrograman java yang dimana ia digunakan ketika kita membutuhkan sebuah bilangan bulat yang besar
