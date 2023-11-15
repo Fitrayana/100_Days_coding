8591f7541759bd2f79f1674dbefaf69536975524
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-15 20:47:40 +0800

    Day38

---
+++ Perkalian_TipeData_Short.java
@@ -0,0 +1,11 @@
+
+public class perkaliann_short {
+    public static void main(String[] args) {
+        short a = 350;
+        short b = 100;
+        
+        short perkalian = (short)(a*b);
+        
+        System.out.println("hasil: "+perkalian);
+    }
+}
