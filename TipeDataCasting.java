293715de2562b3cda17be0ca264ea22021ba06b7
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-16 23:14:29 +0800

    Day39

---
+++ TipeDataCasting.java
@@ -0,0 +1,12 @@
+public class Casting {
+    public static void main(String[] args) {
+        
+        double doubleNilai = 10.5;
+        int intNilai = (int) doubleNilai;
+
+        System.out.println("Double Nilai: " + doubleNilai);
+        System.out.println("Integer Nilai: " + intNilai);
+    }
+}
+
+//Tipe data casting merupakan tipe data non primitif yang dimana ia merupakan suatu proses mengubah nilai dari satu tipe data ke tipe data lainnya.
