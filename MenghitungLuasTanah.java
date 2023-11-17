79fe2e87c44f9ccad35d9d99afb5478fed198c14
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-17 22:16:16 +0800

    Day39

---
+++ MenghitungLuasTanah.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class HitungLuasTanah {
+    public static void main(String[] args) {
+        // disini kita menggunakan objek Scanner untuk input dari user/pengguna
+        Scanner scanner = new Scanner(System.in);
+
+        // input panjang dan lebar tanah
+        System.out.print("Masukkan panjang tanah (meter): ");
+        double panjang = scanner.nextDouble();
+
+        System.out.print("Masukkan lebar tanah (meter): ");
+        double lebar = scanner.nextDouble();
+
+        // menghitung luas tanah dengan rumus dibawah
+        double luas = panjang * lebar;
+      
+        System.out.println("Luas tanah adalah: " + luas + " meter persegi");
+
+  
+        scanner.close();
+    }
+}
