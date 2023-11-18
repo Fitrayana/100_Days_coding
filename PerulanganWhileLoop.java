705e1e421f67ca9d619f4f7ccfd4095a7d0fa662
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-18 21:27:16 +0800

    Day41

---
+++ PerulanganWhileLoop.java
@@ -0,0 +1,10 @@
+public class fitra {
+    public static void main(String[] args) {
+        int f = 0;
+        
+        while (f < 5) {
+            System.out.println("Indeks ke-" + f);
+            i++;
+        }
+    }
+}
