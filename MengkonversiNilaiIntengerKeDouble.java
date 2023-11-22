e04d82a71fabbfdba43e6c91a4c712706ffbb96e
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-22 22:43:44 +0800

    Day45

---
+++ MengkonversiNilaiIntengerKeDouble.java
@@ -0,0 +1,14 @@
+
+public class fitra {
+    public static void main(String[] args) {
+        // Deklarasikan variabel
+        int nilaiInt = 20;
+
+        // Konversi int ke double
+        double nilaiDouble = (double) nilaiInt;
+
+        // hasil konversi
+        System.out.println("Nilai int: " + nilaiInt);
+        System.out.println("Nilai double setelah konversi: " + nilaiDouble);
+    }
+}
