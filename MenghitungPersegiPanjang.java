ac84195eeec78bca7759d3a4a81bd78a14f8e879
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-26 22:09:02 +0800

    Day49

---
+++ MenghitungPersegiPanjang.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class fitra{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang persegi panjang: ");
+        double panjang = scanner.nextDouble();
+
+        System.out.print("Masukkan lebar persegi panjang: ");
+        double lebar = scanner.nextDouble();
+
+        double luas = panjang * lebar;
+        double keliling = 2 * (panjang + lebar);
+
+        System.out.println("Luas persegi panjang: " + luas);
+        System.out.println("Keliling persegi panjang: " + keliling);
+
+       
+    }
+}
+
