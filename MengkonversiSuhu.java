ab0d2fa77a2da6990c70c3148fca0992bfd61f6a
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-11-27 21:31:11 +0800

    Day50

---
+++ MengkonversiSuhu.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+public class fitra {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);    
+        System.out.print("Masukkan suhu dalam Celsius: ");
+        double suhuCelsius = scanner.nextDouble();
+        // Melakukan konversi suhu dari Celsius ke Fahrenheit
+        double suhuFahrenheit = (suhuCelsius * 9/5) + 32;
+        System.out.println("Suhu dalam Fahrenheit: " + suhuFahrenheit);
+
+    }
+
+}
