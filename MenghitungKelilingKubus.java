52619acde16538270a0d74f4c5bc3d1a5109e0d6
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-03 20:53:10 +0800

    Day56

---
+++ MenghitungKelilingKubus.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class fitra {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Panjang Sisi Kubus: ");
+        double sisi = sc.nextDouble();
+        double keliling = 12 * sisi;
+        
+        System.out.println("Keliling Kubus Adalah: " + keliling);
+        
+        sc.close();
+    }
+    
+}
