56de28e2bce8e6f136ba8b322d1fcb848b5cb5d5
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-06 21:52:16 +0800

    Day59

---
+++ MenghitungKelilingBola.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class fitra{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jari-jari bola: ");
+        double jariJari = scanner.nextDouble();
+
+        // Menghitung keliling bola menggunakan rumus K = 2 * pi * r
+        double keliling = 2 * Math.PI * jariJari;
+
+        // Menampilkan hasil keliling bola
+        System.out.println("Keliling bola adalah: " + keliling);
+    }
+}
+
