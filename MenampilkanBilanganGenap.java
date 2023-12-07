4470fb406678ceb3e7ff6b91d7bc1a7f152896fc
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-07 20:27:36 +0800

    Day60

---
+++ MenampilkanBilanganGenap.java
@@ -0,0 +1,12 @@
+public class fitra {
+    public static void main(String[] args) {
+    
+        for (int i = 1; i < 50; i+=2) {
+            System.out.println(i);
+            
+        }
+
+        
+    }
+    
+}
