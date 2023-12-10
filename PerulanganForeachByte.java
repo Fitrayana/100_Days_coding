fa38ea005db0a4d44c1cfa7a04daeb2940c3b0d5
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-10 22:51:45 +0800

    Day63

---
+++ PerulanganForeachByte.java
@@ -0,0 +1,15 @@
+
+public class fitra{
+
+    public static void main(String[] args) {
+
+        byte angka[] = {30, 61, 42, 13, 44};
+
+        for (byte x : angka) {
+            System.out.print(x + " ");
+
+        }
+        System.out.println("");
+    }
+
+}
