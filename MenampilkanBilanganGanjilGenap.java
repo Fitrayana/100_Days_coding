42a48fd995fa64df5fc091690784a88e12caee4a
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-11 22:38:07 +0800

    Day64

---
+++ MenampilkanBilanganGanjilGenap.java
@@ -0,0 +1,14 @@
+
+public class fitra {
+
+    public static void main(String[] args) {
+     
+    int number = 20;
+
+    if (number % 2 == 0) {
+      System.out.println(number + " bilangan genap");
+    } else {
+      System.out.println(number + " bilangan ganjil");
+    }
+  }
+}
