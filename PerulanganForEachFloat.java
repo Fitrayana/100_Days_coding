141240d75f97f4520580a44423ead6d9d27fdcf2
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-15 21:09:40 +0800

    Day68

---
+++ PerulanganForEachFloat.java
@@ -0,0 +1,13 @@
+public class fitra {
+    public static void main(String[] args) {
+        float angka[] = {32110, 62331, 4242, 135533, 44434};
+
+        for (float x : angka) {
+            System.out.print(x + " ");
+
+        }
+        System.out.println("");
+    
+    }
+    
+}
