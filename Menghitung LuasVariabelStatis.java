0678d722e31bb7025ab3c4ccc1ee38d3b66f50ab
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-16 21:51:28 +0800

    Day69

---
+++ Menghitung LuasVariabelStatis.java
@@ -0,0 +1,17 @@
+
+public class fitra {
+
+    public static void main(String[] args) {
+       
+        int a = 1;
+        int b = 2;
+        String c = "Hello, world!";
+        int jumlahVariabel = 0;
+
+        jumlahVariabel++;
+        jumlahVariabel++;
+        jumlahVariabel++;
+
+        System.out.println("Jumlah variabel: " + jumlahVariabel);
+    }
+}
