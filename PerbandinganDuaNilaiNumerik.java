6ca56e34f47822587ce7eca50c2857154651dbd6
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-17 22:06:59 +0800

    Day70

---
+++ PerbandinganDuaNilaiNumerik.java
@@ -0,0 +1,21 @@
+public class fitra {
+
+    public static void main(String[] args) {
+    int a = 40;
+    int b = 20;
+
+System.out.println(a == b); 
+
+System.out.println(a != b); 
+
+System.out.println(a < b); 
+
+System.out.println(a > b); 
+
+System.out.println(a <= b); 
+
+System.out.println(a >= b); 
+
+    }
+    
+}
