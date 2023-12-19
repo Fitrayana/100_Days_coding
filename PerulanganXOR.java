9900a79bb173523da2eebf5ef7fc14a46e394b8b
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-19 21:47:01 +0800

    Day72

---
+++ PerulanganXOR.java
@@ -0,0 +1,16 @@
+public class fitra {
+
+    public static void main(String[] args) {
+       
+        boolean a = true;
+        boolean b = false;
+
+        boolean c = a ^ b;
+
+        if (c) {
+            System.out.println("Nilai XOR adalah true");
+        } else {
+            System.out.println("Nilai XOR adalah false");
+        }
+    }
+}
