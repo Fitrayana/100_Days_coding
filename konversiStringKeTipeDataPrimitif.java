ea53dc759ff22795b8606a68c243a50da1ddaf22
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-20 23:03:19 +0800

    Day73

---
+++ konversiStringKeTipeDataPrimitif.java
@@ -0,0 +1,27 @@
+public class fitra {
+   
+    public static void main(String[] args) {
+     
+        String bilanganString;
+        int bilanganInt;
+        float bilanganFloat;
+        double bilanganDouble;
+        boolean booleanValue;
+        
+        bilanganString = "100";
+        bilanganInt = Integer.parseInt(bilanganString);
+        bilanganFloat = Float.parseFloat(bilanganString);
+        bilanganDouble = Double.parseDouble(bilanganString);
+        
+        String booleanString = "true";  
+        booleanValue = Boolean.parseBoolean(booleanString);
+        
+        System.out.println("bilangan int = " + bilanganInt);
+        System.out.println("bilangan float = " + bilanganFloat);
+        System.out.println("bilangan double = " + bilanganDouble);
+        System.out.println("bilangan boolean = " + booleanValue);
+    }
+}
+
+              
+      
