8099b77fc712f17198f4f09b05dc8fdaf7651494
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-21 22:28:00 +0800

    Day74

---
+++ PenjumlahanBilanganGanjil.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class fitra {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan nilai pertama: ");
+        int nilai1 = sc.nextInt();
+        int k = 0;
+        for (int n = 0; n < nilai1; n++) {
+            if (n % 2 == 1) {
+                k += n;
+            }
+        }
+        System.out.println("jumlah seluru dibawa variabel inputan: " + k);
+    }
+}
