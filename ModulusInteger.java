059168d7419ed18daa476e30557e8f97995e3019
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-24 21:37:32 +0800

    Day77

---
+++ ModulusInteger.java
@@ -0,0 +1,11 @@
+
+public class fitra {
+    public static void main(String[] args) {
+        Integer mod=432;
+        Integer ulus=3;
+        
+        Integer modulus=mod%ulus;
+        
+        System.out.println("modulus Nonprimitif Integer: "+modulus);
+    }
+}
