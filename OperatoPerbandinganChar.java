a885f350ae1f5934618e08870e4bb5cfe06a47bd
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-30 21:26:12 +0800

    Day83

---
+++ OperatoPerbandinganChar.java
@@ -0,0 +1,13 @@
+public class fitra {
+    public static void main(String[] args) {
+        char a = 'A';//deklarasi variabel char
+        char b = 'B';
+        
+        System.out.println("A == B "+(a== b));//false
+        System.out.println("A != B  "+(a != b));//true
+        System.out.println("A < B  "+( a < b));//false
+        System.out.println("A > B  "+( a > b));//true
+        System.out.println("A >= B "+(a >= b));//false
+        System.out.println("A <= B "+ (a <= b));//true
+    }
+}
