4b677b35b6b0f9475d97b6d441a49dfad258ef89
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2023-12-31 22:16:13 +0800

    Day84

---
+++ CekKataPalindrom.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class fitra {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah kata: ");
+
+        String kata = scanner.nextLine();
+
+        // Memanggil metode untuk menentukan apakah kata tersebut palindrom
+
+        boolean Palindrome = kata.equals(new StringBuilder(kata).reverse().toString())
+
+
+        if (Palindrome) {
+
+            System.out.println("Kata \"" + kata + "\" adalah palindrom.");
+
+        } else {
+
+            System.out.println("Kata \"" + kata + "\" bukan palindrom.");
+
+        }
+
+    }
+
+}
+
