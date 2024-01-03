b1fb09ee4530ed16fd5b0d9cca64c3071866030a
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2024-01-03 22:26:30 +0800

    Day87

---
+++ NonPrimitiCharacter.java
@@ -0,0 +1,8 @@
+public class ftra {
+    public static void main(String[] args) {
+        Character aku='A';
+        
+        
+        System.out.println("nilai character nilai aku: "+aku );
+    }
+}
