183fc50c15a73630cbeed5330e0e2cbfcaf7eb86
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2024-01-04 22:04:53 +0800

    Day88

---
+++ MerepresentasikanMahasiswa.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ftra {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.print("Masukkan nama Mahasiswa: ");
+        String namaMahasiswa = scanner.nextLine();
+        
+        System.out.print("Masukkan nim mahasiswa: ");
+        String nimMahasiswa = scanner.nextLine();
+        
+        System.out.print("Masukkan ipk Mahasiswa: ");
+        double ipkMahasiswa = scanner.nextDouble();
+        
+        System.out.println("Nama mahasiswa: " + namaMahasiswa);
+        System.out.println("Nim mahasiswa: " + nimMahasiswa);
+        System.out.println("IPK mahasiswa: " + ipkMahasiswa);
+    }
+}
