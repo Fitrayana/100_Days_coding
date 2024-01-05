ea9ab1b84fa58b54c8db16c7c89c982ff90024c4
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2024-01-05 22:33:14 +0800

    Day89

---
+++ PenjumlahanBigDecimal.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+
+
+public class fitra{
+    public static void main(String[] args) {
+        BigDecimal tam= new BigDecimal("1111111111111111.2222222222222222222222");
+        BigDecimal bah= new BigDecimal("3333333333333333.4444444444444444444444");
+        
+        BigDecimal tambah =tam.add(bah);
+        
+        System.out.println("hasil penjumlahan BigDecimal: "+tambah);
+    }
+}
