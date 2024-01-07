eef02a5446ff76f1bf2814d91d38dda722d872ec
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2024-01-07 23:06:05 +0800

    Day91

---
+++ MenghitungAngkaMundur.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class fitra {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Masukkan angka: ");
+
+        int bilangan = scanner.nextInt();
+
+        System.out.println("hitungan mundur dari angka " + bilangan + " tersebut adalah");
+
+        for (int i = bilangan; i >= 0; i--) {
+
+            System.out.println(i + " ");
+
+        }
+
+    }
