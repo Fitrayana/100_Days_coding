2a2b6e56bba8d80f542b72c394b92b9c165bd144
Fitrayana <99769407+Fitrayana@users.noreply.github.com>
2024-01-09 22:07:43 +0800

    Day93

---
+++ PerkalianDuaBilanganBulat.java
@@ -0,0 +1,9 @@
+public class fitra {
+    public static void main(String[] args) {
+        int a = 15;//DEKLARASI variabel
+        int b = 25;
+        int hasil = a*b;//perkalian
+        System.out.println("hasil perkalian " + a + " dan " + b + " adalah " + hasil);
+    }
+ 
+}
